b4d996e3f5a48f84ef65a874f6f39a95c448a939
samiramrullah <samiramrullah@gmail.com>
2025-11-30 22:00:37 +0530

    p visible

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -91,7 +91,7 @@ const Page = () => {
                   </label>
                   <input
                     name="password"
-                    type="password"
+                    type="text"
                     required
                     className="block w-full rounded-lg bg-blue-50 px-4 py-2 text-base border border-transparent focus:border-blue-500"
                     onChange={onChangeHandler}
