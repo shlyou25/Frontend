ea50f0ca127aa8d7abf1f8d77f6070e36fc0958a
Samiramrullah <samiramrullah@gmail.com>
2026-01-10 19:58:32 +0530

    plan added

---
+++ app/plan/PlanRequest.tsx
@@ -0,0 +1,110 @@
+"use client";
+
+import React, { useState } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
+
+interface PlanRequestProps {
+  onClose: () => void;
+}
+
+const plan = [
+  { title: "Starter", price: 0.99, per: "Month", feature: 5 },
+  { title: "Basic", price: 4.99, per: "Month", feature: 100 },
+  { title: "Business", price: 9.99, per: "Month", feature: 500 },
+  { title: "Premium", price: 14.99, per: "Month", feature: 1000 },
+  { title: "Platinum", price: 19.99, per: "Month", feature: 2000 },
+  { title: "Gold", price: 24.99, per: "Month", feature: 5000 },
+];
+
+const PlanRequest = ({ onClose }: PlanRequestProps) => {
+  const [selectedPlan, setSelectedPlan] = useState("");
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const onSubmitHandler = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setError("");
+
+    if (!selectedPlan) {
+      setError("Please select a plan");
+      return;
+    }
+
+    try {
+      setLoading(true);
+     const res= await axios.post(
+       `${process.env.NEXT_PUBLIC_apiLink}planrequest/addplanrequest`,
+        { planTitle: selectedPlan },
+        { withCredentials: true } 
+      );
+    toast.success(res.data.message);
+      onClose();
+    } catch (err: any) {
+        toast.error(err.response.data.message);
+      if (err.response?.status === 401) {
+        setError("Please login first to proceed");
+      } else if (err.response?.status === 403) {
+        setError("Your account is inactive");
+      } else {
+        setError("Failed to submit plan request");
+      }
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <section className="max-w-4xl p-6 mx-auto bg-white rounded-md shadow-md">
+      <h2 className="text-lg font-semibold text-gray-700">
+        Plan Request
+      </h2>
+
+      <form onSubmit={onSubmitHandler}>
+        <div className="mt-4">
+          <select
+            value={selectedPlan}
+            onChange={(e) => setSelectedPlan(e.target.value)}
+            className="block w-full px-4 py-2 text-gray-700 bg-white border border-gray-200 rounded-md
+            focus:border-blue-400 focus:ring-2 focus:ring-blue-300 focus:outline-none"
+          >
+            <option value="">Select a plan</option>
+            {plan.map((p) => (
+              <option key={p.title} value={p.title}>
+                {p.title} — ${p.price}/{p.per} ({p.feature} domains)
+              </option>
+            ))}
+          </select>
+        </div>
+
+        {error && (
+          <p className="mt-3 text-sm text-red-600">
+            {error}
+          </p>
+        )}
+
+        <div className="flex justify-end gap-3 mt-6">
+          <button
+            type="button"
+            onClick={onClose}
+            className="px-6 py-2 text-sm border rounded-md"
+          >
+            Cancel
+          </button>
+
+          <button
+            type="submit"
+            disabled={loading}
+            className={`px-8 py-2.5 text-sm text-white rounded-md
+              ${loading ? "bg-gray-400" : "bg-gray-700 hover:bg-gray-600"}
+            `}
+          >
+            {loading ? "Submitting..." : "Save"}
+          </button>
+        </div>
+      </form>
+    </section>
+  );
+};
+
+export default PlanRequest;
--- app/plan/plancard.tsx
+++ app/plan/plancard.tsx
@@ -1,6 +1,8 @@
 "use client";
-import { useRouter } from 'next/navigation';
 import Image from 'next/image'
+import Modal from '@/components/model';
+import { useState } from 'react';
+import PlanRequest from './PlanRequest';
 
 export interface PlanCardInterface {
     title: string;
@@ -13,32 +15,43 @@ export interface PlanCardInterface {
 
 
 const Plancard = ({ title, price, per, feature }: PlanCardInterface) => {
-    const router=useRouter();
-    const buyPlanHandler = () => {
-    router.push(`/payment?plan=${encodeURIComponent(title)}`);
-  };
+    const [open,setOpen]=useState(false);
+    // const router=useRouter();
+    // const buyPlanHandler = () => {
+    // router.push(`/payment?plan=${encodeURIComponent(title)}`);
 
     return (
-        <div className="relative bg-linear-to-b from-[#F6F9FF] to-[#EEF3FF] rounded-4xl shadow-[0_4px_24px_rgba(146,151,255,0.12)] border border-[#EBEEF8] p-8 w-full max-w-xs flex flex-col items-start mx-auto ">
-            <div className="absolute top-4 left-4">
-                <div className="bg-white w-12 h-12 flex items-center justify-center rounded-full shadow-[0_1px_6px_rgba(166,178,255,0.15)]">
-                    <Image src="/assets/icons/arrow-right.webp" alt="arrow" height={14} width={14} />
+        <>
+            <div className="relative bg-linear-to-b from-[#F6F9FF] to-[#EEF3FF] rounded-4xl shadow-[0_4px_24px_rgba(146,151,255,0.12)] border border-[#EBEEF8] p-8 w-full max-w-xs flex flex-col items-start mx-auto ">
+                <div className="absolute top-4 left-4">
+                    <div className="bg-white w-12 h-12 flex items-center justify-center rounded-full shadow-[0_1px_6px_rgba(166,178,255,0.15)]">
+                        <Image src="/assets/icons/arrow-right.webp" alt="arrow" height={14} width={14} />
+                    </div>
                 </div>
+                <div className="w-full mt-12 mb-4 text-left">
+                    <span className="font-semibold text-xl text-[#1C1E21]">{title}</span>
+                </div>
+                <div className="flex items-end w-full mb-1">
+                    <span className="text-[2rem] font-bold text-black leading-tight">{`$ ${price}`}</span>
+                    <span className="text-lg text-[#707277] ml-2 pb-1">/ {per}</span>
+                </div>
+                <div className="w-full text-base text-[#26282C] mb-3 text-left">{feature} Names</div>
+                <button className="w-full bg-[#2865E8] rounded-full text-white font-medium py-3 text-base hover:bg-[#1f51bd] transition mt-2 text-left pl-5 cursor-pointer"
+                onClick={()=>setOpen(true)}
+                >
+                    Start 30 Day Free Trial
+                </button>
             </div>
-            <div className="w-full mt-12 mb-4 text-left">
-                <span className="font-semibold text-xl text-[#1C1E21]">{title}</span>
-            </div>
-            <div className="flex items-end w-full mb-1">
-                <span className="text-[2rem] font-bold text-black leading-tight">{`$ ${price}`}</span>
-                <span className="text-lg text-[#707277] ml-2 pb-1">/ {per}</span>
-            </div>
-            <div className="w-full text-base text-[#26282C] mb-3 text-left">{feature} Names</div>
-            <button className="w-full bg-[#2865E8] rounded-full text-white font-medium py-3 text-base hover:bg-[#1f51bd] transition mt-2 text-left pl-5 cursor-pointer"
-                onClick={buyPlanHandler}
+            <Modal
+                isOpen={open}
+                onClose={() => setOpen(false)}
+                title="Plan Request"
             >
-                Start 30 Day Free Trial
-            </button>
-        </div>
+                <PlanRequest
+                    onClose={() => setOpen(false)}
+                />
+            </Modal>
+        </>
     )
 }
 export default Plancard
