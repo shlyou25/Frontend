8806c250edd7c4918ad0614eefa1a44b139a24c3
Samiramrullah <samiramrullah@gmail.com>
2026-01-17 22:23:51 +0530

    filter added

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '../../components/Navbar'
 import AboutDomz from './About'
+import Footer from '@/components/Footer'
 
 const page = () => {
   return (
     <div>
       <Navbar/>
       <AboutDomz/>
+      <Footer/>
     </div>
   )
 }
